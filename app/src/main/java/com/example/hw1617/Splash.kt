5cbcf0cb4c2efac37b503121ae333fd55bc53541
Sergey Tytyanchuk <xsergey87x@ukr.net>
2021-08-31 21:26:28 +0300

    First commit

---
+++ app/src/main/java/com/example/hw1617/Splash.kt
@@ -0,0 +1,20 @@
+package com.example.hw1617
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            startActivity(Intent(this, MainActivity::class.java))
+
+        }, 3000)
+    }
+}
